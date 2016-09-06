6c706c9c22263fafa73a9e36fe68abad56a0ad8d
Matt Powley <mattpow35@gmail.com>
2016-09-06 09:56:37 -0600

    made it less stupid by displaying text

---
+++ src/stupid/control/StupidController.java
@@ -0,0 +1,9 @@
+package stupid.control;
+
+public class StupidController {
+	public void start()
+	{
+		System.out.println("this program is less stupid");
+	}
+
+}
---
+++ src/stupid/control/StupidRunner.java
@@ -0,0 +1,10 @@
+package stupid.control;
+
+public class StupidRunner {
+	public static void main (String [] args)
+	{
+		StupidController firstAppController = new StupidController();
+		firstAppController.start();
+	}
+
+}
