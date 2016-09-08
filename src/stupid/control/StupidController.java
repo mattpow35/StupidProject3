61444c5e50f76a63b30ccb195f5b8e87ec48fa9c
Matt Powley <mattpow35@gmail.com>
2016-09-08 09:25:21 -0600

    added the ability to input info with the keyboard to answer a question and then have it reply

--- src/stupid/control/StupidController.java
+++ src/stupid/control/StupidController.java
@@ -1,19 +1,30 @@
 package stupid.control;
 
+import java.util.Scanner;
+
 public class StupidController 
 {
-	private String myName; 
+	private String myName;
+	private Scanner inputReader;
 	
 	public StupidController()
 	{
 		myName = "Matt Powley"; 
-			
+		inputReader = new Scanner(System.in);	
 	}
 	
 	public void start()
 	{
 		System.out.println("this program is less stupid");
 		System.out.println(myName);
+		askQuestions();
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("How was your day?");
+		String input = inputReader.nextLine();
+		System.out.println("Oh, you said: " + input);
 	}
 
 }
