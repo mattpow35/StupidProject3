d5750eaa5a71ec0993885f4458fe6c41a1877f89
Matt Powley <mattpow35@gmail.com>
2016-09-12 08:46:47 -0600

    added question about age

--- src/stupid/control/StupidController.java
+++ src/stupid/control/StupidController.java
@@ -25,6 +25,15 @@ public class StupidController
 		System.out.println("How was your day?");
 		String input = inputReader.nextLine();
 		System.out.println("Oh, you said: " + input + " about your day.");
+		
+		System.out.println("My next question is this: What is the best type of food?");
+		String foodInput = inputReader.nextLine();
+		System.out.println("OK, you typed this: " + foodInput);
+		
+		System.out.println("How old are you?");
+		int ageInput = inputReader.nextInt();
+		System.out.println("Oh you are only " + ageInput + " years old, you are so young."); 
+				
 	}
 
 }
